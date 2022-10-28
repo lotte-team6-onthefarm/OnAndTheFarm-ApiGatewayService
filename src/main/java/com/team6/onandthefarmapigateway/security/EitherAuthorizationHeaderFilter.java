d2878999999039dd0711223bf168601f14c740a0
ksh940925 <ksh9409255@naver.com>
2022-10-28 09:28:46 +0900

    feat: 해더가 있고 없고를 둘다 걸러주는 필터 추가

---
+++ src/main/java/com/team6/onandthefarmapigateway/security/EitherAuthorizationHeaderFilter.java
@@ -0,0 +1,125 @@
+package com.team6.onandthefarmapigateway.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class EitherAuthorizationHeaderFilter extends AbstractGatewayFilterFactory<EitherAuthorizationHeaderFilter.Config> {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    Environment env;
+
+
+    public EitherAuthorizationHeaderFilter(
+                                     JwtTokenUtil jwtTokenUtil,
+                                     Environment env){
+        super(EitherAuthorizationHeaderFilter.Config.class);
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.env = env;
+    }
+
+    public static class Config {
+        // Put configuration properties here
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            ServerWebExchange serverWebExchange = null;
+            if(request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                String accessToken = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+
+                if(accessToken != null) {
+                    try{
+                        String role = jwtTokenUtil.getRole(accessToken);
+                        if(role.equals("ROLE_ADMIN")){
+                            Long sellerId = jwtTokenUtil.getId(accessToken);
+                            System.out.println("jwt 인증 필터에서 토큰 속 셀러 정보 : "+ sellerId);
+
+                            if(sellerId == null){
+                                throw new IllegalArgumentException("정보가 담겨있지 않은 빈 토큰입니다.");
+                            }
+
+                            // 4. 토큰 유효성 검증
+                            if (jwtTokenUtil.validateToken(accessToken)) {
+                                // Authentication이 아닌 헤더에 유저 정보를 담아서 보내기
+                                ServerHttpRequest newRequest = request.mutate()
+                                        .header("memberId", Long.toString(sellerId))
+                                        .header("memberRole", "seller").build();
+
+                                serverWebExchange = exchange.mutate().request(newRequest).build();
+                            }
+                        }
+                        else{
+                            Long userId = jwtTokenUtil.getId(accessToken);
+                            System.out.println("jwt 인증 필터에서 토큰 속 사용자 정보 : "+userId);
+
+                            if (userId == null) {
+                                throw new IllegalArgumentException("정보가 담겨있지 않은 빈 토큰입니다.");
+                            }
+
+                            // 4. 토큰 유효성 검증
+                            if (jwtTokenUtil.validateToken(accessToken)) {
+                                // Authentication이 아닌 헤더에 유저 정보를 담아서 보내기
+                                ServerHttpRequest newRequest = request.mutate()
+                                        .header("memberId", Long.toString(userId))
+                                        .header("memberRole", "user").build();
+
+                                serverWebExchange = exchange.mutate().request(newRequest).build();
+                            }
+                        }
+                    }
+                    catch (SignatureException ex) {
+                        log.error("AuthorizationHeaderFilter - 유효하지 않은 JWT 서명입니다.");
+                        throw new SignatureException("유효하지 않은 JWT 서명입니다.");
+                    } catch (MalformedJwtException ex) {
+                        log.error("AuthorizationHeaderFilter - 올바르지 않은 JWT 토큰입니다.");
+                        throw new MalformedJwtException("올바르지 않은 JWT 토큰입니다.");
+                    } catch (ExpiredJwtException ex) {
+                        log.error("AuthorizationHeaderFilter - 만료된 JWT 토큰입니다.");
+                        throw new NullPointerException("만료된 JWT 토큰입니다.");
+                    } catch (UnsupportedJwtException ex) {
+                        log.error("AuthorizationHeaderFilter - 지원하지 않는 형식의 JWT 토큰입니다.");
+                        throw new UnsupportedJwtException("지원하지 않는 형식의 JWT 토큰입니다.");
+                    } catch (IllegalArgumentException ex) {
+                        log.error("AuthorizationHeaderFilter - 정보가 담겨있지 않은 빈 토큰입니다.");
+                        throw new IllegalArgumentException("정보가 담겨있지 않은 빈 토큰입니다.");
+                    } catch (Exception ex) {
+                        log.error("AuthorizationHeaderFilter - 올바르지 않은 JWT 토큰입니다.");
+                    }
+                }
+                return chain.filter(serverWebExchange);
+            }
+            return chain.filter(exchange.mutate().build());
+
+            //return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String message, HttpStatus httpStatus) {
+
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        log.error(message);
+        return response.setComplete();
+    }
+
+}
